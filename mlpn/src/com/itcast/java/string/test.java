9ba6507e1653f7540b7881506c140aaf0af5c29c
Adamusi <cnu10028@163.com>
2021-01-29 22:40:54 +0800

    String转int方法

---
+++ mlpn/src/com/itcast/java/string/test.java
@@ -0,0 +1,18 @@
+package com.itcast.java.string;
+
+/**
+ * @author 张自强
+ * @date 2021-1-29 22:24:28
+ */
+
+public class test {
+
+	public static void main(String[] args) {
+		// 5. 补充知识点字符串转int方法
+		String string = "123";
+		int a = Integer.parseInt(string);
+		System.out.println(a);
+
+	}
+
+}
