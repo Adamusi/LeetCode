f2edf266f60c35aab43fc5bc193d458e21bead8e
Adamusi <cnu10028@163.com>
2021-02-21 21:05:09 +0800

    2021-2-21盛最多水的容器

--- LeetCode/src/Array/TwoSum_01.java
+++ LeetCode/src/Array/TwoSum_01.java
@@ -1,4 +1,4 @@
-package code;
+package Array;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -6,9 +6,6 @@ import java.util.Map;
 /**
  * @author zzq
  * @date 2020-10-9 21:23:09
- *  1. Two Sum 题意：返回数组中和为给定数的下标 难度：Easy 分类：Array,
- *  HashTable 算法：题目说明了数组中一定有解，且解唯一，所以用哈希表记录已遍历的元素即可
-   *    给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标
  */
 
 public class TwoSum_01 {
@@ -23,9 +20,13 @@ public class TwoSum_01 {
 		System.out.println(r[1]);
 	}
 
+	/**
+	 *   哈希查找
+	 */
 	private static int[] twoSum(int[] nums, int target) {
 		Map<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < nums.length; i++) {
+			  // 哈希查找 - O(1)
 			if (map.containsKey(target - nums[i])) {
 				return new int[] { map.get(target - nums[i]), i };
 			}
